e5c0a580577e89526e209d4333fcc19b9983cb8e
Dev-Dhiraj <dhiraj1221gfdg@gmail.com>
2025-12-31 08:50:57 +0530

    some good things 0.1

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-
+ 
 const user = document.getElementById("user");
 const work = document.getElementById("work");
 //some function
@@ -6,11 +6,11 @@ function using() {
     const username = prompt("what is your goood name");
     const workload = prompt("what do you do");
     const forStudents = "how can I help you as a tutor";
-    const intro = "I am a ai image tutor ui"
+    const intro = "I am a ai image tutor ui";
     if (workload = "student") {
         work.innerText = forStudents;
-        console.log(hug);
-    }else () {
+        console.log(intro)
+    } else () {
         work.innerText = intro;
     };
     user.innerText = username;
@@ -20,6 +20,7 @@ using();
 
 const image = document.getElementsByClassName("img")[0];
 console.log(image);
+
 
 
 
