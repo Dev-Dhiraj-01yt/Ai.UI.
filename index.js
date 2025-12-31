126a0d869cb0d97a1503ea54f1a147a50dcc8032
Dev-Dhiraj <dhiraj1221gfdg@gmail.com>
2025-12-31 08:42:35 +0530

    Refactor using function and improve code structure

--- index.js
+++ index.js
@@ -5,16 +5,18 @@ const work = document.getElementById("work");
 function using() {
     const username = prompt("what is your goood name");
     const workload = prompt("what do you do");
-    const hug = "how can I help you as a tutor"
-    user.innerText = username;
-    
-    if(workload == "student") {
+    const hug = "how can I help you as a tutor";
+    if (workload === "student") {
         work.innerText = hug;
         console.log(hug);
     };
+    user.innerText = username;
 };
+
 using();
+
 const image = document.getElementsByClassName("img")[0];
 console.log(image);
+
 
 
