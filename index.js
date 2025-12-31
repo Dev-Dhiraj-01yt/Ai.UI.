cc56e69a1e471d0ba89756561e02df8e5a55b3cd
Dev-Dhiraj <dhiraj1221gfdg@gmail.com>
2025-12-31 08:16:19 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+
+const user = document.getElementById("user");
+const work = document.getElementById("work");
+//some function
+function using() {
+    const username = prompt("what is your goood name");
+    const workload = prompt("what do you do");
+    const hug = "How can Ihelp you with"
+    user.innerText = username;
+    work.innerText = ;
+};
+using();
+const image = document.getElementsByClassName("img")[0];
+console.log(image);
