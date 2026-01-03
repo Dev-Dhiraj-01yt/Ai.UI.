345384ad0ea07e3f4825da7f4b793a3fe4ed6565
Dev-Dhiraj <dhiraj1221gfdg@gmail.com>
2026-01-03 11:29:01 +0530

    Minors

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-
+/*
 const user = document.getElementById("user");
 const work = document.getElementById("work");
 //some function
@@ -37,7 +37,47 @@ using()
 ChalBeta();
 
 const image = document.getElementsByClassName("img")[0];
+console.log(image);*/
+
+const user = document.getElementById("user");
+const work = document.getElementById("work");
+function using() {
+    const username = prompt("what is your good name");
+    const workload = prompt("what do you do");
+    const forStudents = "how can I help you as a tutor";
+    const intro = "I am a ai image tutor ui";
+    let nameArray = ["student","Student","STUDENT"];
+    let logic;
+    
+    function ChalBeta() {
+        if (workload == nameArray[0]) {
+            logic = 1;
+        } else if (workload == nameArray[1]) {
+            logic = 1;
+        } else if (workload == nameArray[2]) {
+            logic = 1;
+        } else {
+            logic = 0;
+        }
+    }
+    
+    ChalBeta(); // Actually call the function!
+    
+    if (logic == 1) {
+        work.innerText = forStudents;
+        console.log(intro);
+    } else {
+        work.innerText = intro;
+    }
+    
+    user.innerText = username;
+}
+
+using();
+
+const image = document.getElementsByClassName("img")[0];
 console.log(image);
+
 
 
 
